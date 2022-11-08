140d685d30be63839ea8d9d2bb103594fbdf39a8
ZILtoid1991 <laszloszeremi@outlook.com>
2022-11-08 12:58:38 +0100

    update types.d

--- source/iota/window/types.d
+++ source/iota/window/types.d
@@ -57,7 +57,7 @@ public class WindowBitmap {
 	}
 	uint		width;		///Width of the bitmap.
 	uint		height;		///Height of the bitmap.
-	size_t		
+	size_t		pitch;
 	///Defines bitmap channels.
 	///lower nibble 0-2 = 2^n bits for that channel
 	///lower nibble 3 = floating-pont number
