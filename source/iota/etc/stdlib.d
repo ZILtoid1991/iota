9d06c137894ad030b00a0e9e3b9eb8e0db07dde5
ZILtoid1991 <laszloszeremi@outlook.com>
2025-12-06 22:38:26 +0100

    Added nogc function to mimic .dup

--- source/iota/etc/stdlib.d
+++ source/iota/etc/stdlib.d
@@ -9,3 +9,9 @@ string fromCSTR(const(char)* cstr) @nogc nothrow {
 	memcpy(result.ptr, cstr, len);
 	return cast(string)result;
 }
+
+T[] mutCopy(T)(const T[] src) @nogc nothrow {
+	T[] result = nu_malloca!T(src.length);
+	memcpy(result.ptr, src.ptr, src.length * T.sizeof);
+	return result;
+}
